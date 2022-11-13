39fca1e987baab0fc84e6c8f70b3da907b3aeda3
sungwon1207 <111910344+sungwon1207@users.noreply.github.com>
2022-11-13 17:53:19 -0500

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,22 @@
+const contractAddress = "0xe9164D65d82A7d3021e199258E1f526c1E606b93"; // Replace with your own contract address
+const chain = 'goerli'; // goerli, polygon, or ethereum
+
+const welcome_h1 = "Welcome to Happy Mad Soul Society!!";
+const welcome_h2 = "Connect to MetaMask to Get Started";
+const welcome_p = 'HMSS a new media company and makes profits from NFTs, Games, Youtube videos, webtoons and various projects. Please check our promotion video <a href="https://youtu.be/28sEP8ZMeBY"target="_blank" rel="noopener noreferrer">HappyMadSoulSociety</a>YouTube channel.';
+const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const h1_presale_mint = "Pre-Sale Minting Open!!";
+const h1_public_mint = "Public Minting Open!!";
+const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const h2_presale_mint = "Public Minting Countdown";
+const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
+const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
+const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const button_presale_coming_soon = "Get on the Whitelist";
+const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const button_presale_already_minted = "Join The Community";
+const button_public_mint = "Mint Your NFT";
+const mint_failed = "Minting failed. 😢 Please try again.";
